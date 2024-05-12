c5f403b2561d548450a96e9481b952538133fad4
Apsara2002 <125apsararanahansi@gmail.com>
2024-05-12 13:01:36 +0530

    search

--- app/src/main/java/com/example/neww4/MainActivity.kt
+++ app/src/main/java/com/example/neww4/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.neww4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.neww4.databinding.ActivityMainBinding
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
     private lateinit var db : ContactDatabaseHelper
     private  lateinit var contactAdapter: ConatctAdapter
+    private var contactList: MutableList<Contact> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +30,30 @@ class MainActivity : AppCompatActivity() {
             val intent =Intent(this,AddContactActivity::class.java)
             startActivity(intent)
         }
+
+        ///////////////////////
+        binding.searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { searchQuery ->
+                    val filteredList = contactList.filter { contact ->
+                        contact.name.contains(searchQuery, ignoreCase = true)
+                    }
+                    contactAdapter.refreshData(filteredList.toMutableList())
+                }
+                return true
+            }
+        })
     }
 
     override fun onResume() {
         super.onResume()
+
+        contactList.clear()
+        contactList.addAll(db.getAllContacts())
         contactAdapter.refreshData(db.getAllContacts())
     }
 }
